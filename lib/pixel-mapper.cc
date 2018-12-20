45dec6f9f163fdb2bc1be2ccd1e2fe0038f3d7b9
silodec-ignacio <43761985+silodec-ignacio@users.noreply.github.com>
2018-12-21 12:38:06 +1300

    Add files via upload

---
+++ lib/pixel-mapper.cc
@@ -0,0 +1,317 @@
+// -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; -*-
+// Copyright (C) 2018 Henner Zeller <h.zeller@acm.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation version 2.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://gnu.org/licenses/gpl-2.0.txt>
+
+#include "pixel-mapper.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <map>
+using namespace std;
+
+  using namespace rgb_matrix;
+  
+  
+
+namespace rgb_matrix {
+namespace {
+
+class RotatePixelMapper : public PixelMapper {
+public:
+  RotatePixelMapper() : angle_(0) {}
+
+  virtual const char *GetName() const { return "Rotate"; }
+
+  virtual bool SetParameters(int chain, int parallel, const char *param) {
+    if (param == NULL || strlen(param) == 0) {
+      angle_ = 0;
+      return true;
+    }
+    char *errpos;
+    const int angle = strtol(param, &errpos, 10);
+    if (*errpos != '\0') {
+      fprintf(stderr, "Invalid rotate parameter '%s'\n", param);
+      return false;
+    }
+    if (angle % 90 != 0) {
+      fprintf(stderr, "Rotation needs to be multiple of 90 degrees\n");
+      return false;
+    }
+    angle_ = (angle + 360) % 360;
+    return true;
+  }
+
+  virtual bool GetSizeMapping(int matrix_width, int matrix_height,
+                              int *visible_width, int *visible_height)
+    const {
+    if (angle_ % 180 == 0) {
+      *visible_width = matrix_width;
+      *visible_height = matrix_height;
+    } else {
+      *visible_width = matrix_height;
+      *visible_height = matrix_width;
+    }
+    return true;
+  }
+
+  virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
+                                  int x, int y,
+                                  int *matrix_x, int *matrix_y) const {
+    switch (angle_) {
+    case 0:
+      *matrix_x = x;
+      *matrix_y = y;
+      break;
+    case 90:
+      *matrix_x = matrix_width - y - 1;
+      *matrix_y = x;
+      break;
+    case 180:
+      *matrix_x = matrix_width - x - 1;
+      *matrix_y = matrix_height - y - 1;
+      break;
+    case 270:
+      *matrix_x = y;
+      *matrix_y = matrix_height - x - 1;
+      break;
+    }
+  }
+
+private:
+  int angle_;
+};
+
+// If we take a long chain of panels and arrange them in a U-shape, so
+// that after half the panels we bend around and continue below. This way
+// we have a panel that has double the height but only uses one chain.
+// A single chain display with four 32x32 panels can then be arranged in this
+// 64x64 display:
+//    [<][<][<][<] }- Raspbery Pi connector
+//
+// can be arranged in this U-shape
+//    [<][<] }----- Raspberry Pi connector
+//    [>][>]
+//
+// This works for more than one chain as well. Here an arrangement with
+// two chains with 8 panels each
+//   [<][<][<][<]  }-- Pi connector #1
+//   [>][>][>][>]
+//   [<][<][<][<]  }--- Pi connector #2
+//   [>][>][>][>]
+class UArrangementMapper : public PixelMapper {
+public:
+  UArrangementMapper() : parallel_(1) {}
+
+  virtual const char *GetName() const { return "U-mapper"; }
+
+  virtual bool SetParameters(int chain, int parallel, const char *param) {
+    if (chain < 2) {  // technically, a chain of 2 would work, but somewhat pointless
+      fprintf(stderr, "U-mapper: need at least --led-chain=4 for useful folding\n");
+      return false;
+    }
+    if (chain % 2 != 0) {
+      fprintf(stderr, "U-mapper: Chain (--led-chain) needs to be divisible by two\n");
+      return false;
+    }
+    parallel_ = parallel;
+    return true;
+  }
+
+  virtual bool GetSizeMapping(int matrix_width, int matrix_height,
+                              int *visible_width, int *visible_height)
+    const {
+    *visible_width = (matrix_width / 64) * 32;   // Div at 32px boundary
+    *visible_height = 2 * matrix_height;
+    if (matrix_height % parallel_ != 0) {
+      fprintf(stderr, "%s For parallel=%d we would expect the height=%d "
+              "to be divisible by %d ??\n",
+              GetName(), parallel_, matrix_height, parallel_);
+      return false;
+    }
+    return true;
+  }
+
+  virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
+                                  int x, int y,
+                                  int *matrix_x, int *matrix_y) const {
+    const int panel_height = matrix_height / parallel_;
+    const int visible_width = (matrix_width / 64) * 32;
+    const int slab_height = 2 * panel_height;   // one folded u-shape
+    const int base_y = (y / slab_height) * panel_height;
+    y %= slab_height;
+    if (y < panel_height) {
+      x += matrix_width / 2;
+    } else {
+      x = visible_width - x - 1;
+      y = slab_height - y - 1;
+    }
+    *matrix_x = x;
+    *matrix_y = base_y + y;
+  }
+
+private:
+  int parallel_;
+};
+
+/******************************/
+/******************************/
+/***** XYFlipped **/
+/******************************/
+/******************************/
+class XYFlipped : public PixelMapper {
+public:
+  XYFlipped() : parallel_(2) {}
+  /*
+	LED Panel is going to be wired up so that the Pi will see it as:
+		--------------> Parallels
+		| [A1] [B1] [C1]
+Chains	| [A2] [B2] [C2]
+		| [A3] [B3] [C3]
+		V [A4] [B4] [C4]
+
+	But we need to map the pixels so that it translates it for humans to see it like:
+	
+			---------------> Chains
+			| [A1] [A2] [A3]
+Parallels	| [B1] [B2] [B3]
+			| [C1] [C2] [C3]
+			V [D1] [D2] [D3]	
+	*/
+  
+  virtual const char *GetName() const { return "xyflipped"; }
+  
+  //These will determine how wide and tall your LED panel is and 
+  //set the x-axis and y-axis limit.
+  //Example: A 3x4 panel with 16 height and 32 width will have a 
+  //x-axis limit of : 0-95. 
+  //y-axis limit of : 0-63.
+  int x_limit, y_limit;
+  
+  virtual bool SetParameters(int chain, int parallel,
+                             const char *parameter_string) {
+	x_limit = (parallel * 32) - 1;
+	y_limit = (chain * 16) - 1;
+    return true;
+  }
+  
+  virtual bool GetSizeMapping(int matrix_width, int matrix_height,
+                              int *visible_width, int *visible_height)
+    const {
+    *visible_width = matrix_width;
+    *visible_height = matrix_height;	
+			
+	return true;
+    }
+	
+ virtual void MapVisibleToMatrix(int matrix_width, int matrix_height,
+                                  int x, int y, 
+                                  int *matrix_x, int *matrix_y) const {
+    
+//Set x-axis and y-axis limit so that only pixels within your 
+//LED panel boundaries will be affected by the change. 
+//Any pixels beyond the scope of your LED panel will not be affected.
+if(x >= 0 && x <= x_limit && y >= 0 && y <= y_limit){
+
+	int Row, Column, xChange, yChange;
+	
+	Column = x / 32;
+	Row = y / 16;
+		
+	xChange = (Row*32) - (Column*32);	
+	x += xChange;
+	
+		if(Column <= 1){
+			yChange = (Row*-16) + (Column*16);
+			y += yChange;
+		}
+
+		//If you have an AB led panel then it will think that each panel has 32 rows of pixels.
+		//This results in the LED panel thinking it has a 'phantom' panel between x64 and x95.
+		//To deal with this we add +16 to the rows (the equivalent of a panel) to skip this phantom panel.
+		else if(Column == 2){
+			yChange = (Row*-16) + (Column*16) + 16;
+			y += yChange;
+		}	
+	*matrix_x = x;
+	*matrix_y = y;	
+}	
+
+}
+private:
+  int parallel_;
+
+};
+/******************************/
+
+typedef std::map<std::string, PixelMapper*> MapperByName;
+static void RegisterPixelMapperInternal(MapperByName *registry,
+                                        PixelMapper *mapper) {
+  assert(mapper != NULL);
+  std::string lower_name;
+  for (const char *n = mapper->GetName(); *n; n++)
+    lower_name.append(1, tolower(*n));
+  (*registry)[lower_name] = mapper;
+}
+
+static MapperByName *CreateMapperMap() {
+  MapperByName *result = new MapperByName();
+
+  // Register all the default PixelMappers here.
+  RegisterPixelMapperInternal(result, new RotatePixelMapper());
+  RegisterPixelMapperInternal(result, new UArrangementMapper());
+  RegisterPixelMapperInternal(result, new XYFlipped());
+  return result;
+}
+
+
+static MapperByName *GetMapperMap() {
+  static MapperByName *singleton_instance = CreateMapperMap();
+  return singleton_instance;
+}
+}  // anonymous namespace
+
+// Public API.
+void RegisterPixelMapper(PixelMapper *mapper) {
+  RegisterPixelMapperInternal(GetMapperMap(), mapper);
+}
+
+std::vector<std::string> GetAvailablePixelMappers() {
+  std::vector<std::string> result;
+  MapperByName *m = GetMapperMap();
+  for (MapperByName::const_iterator it = m->begin(); it != m->end(); ++it) {
+    result.push_back(it->second->GetName());
+  }
+  return result;
+}
+
+const PixelMapper *FindPixelMapper(const char *name,
+                                   int chain, int parallel,
+                                   const char *parameter) {
+  std::string lower_name;
+  for (const char *n = name; *n; n++) lower_name.append(1, tolower(*n));
+  MapperByName::const_iterator found = GetMapperMap()->find(lower_name);
+  if (found == GetMapperMap()->end()) {
+    fprintf(stderr, "%s: no such mapper\n", name);
+    return NULL;
+  }
+  PixelMapper *mapper = found->second;
+  if (mapper == NULL) return NULL;  // should not happen.
+  if (!mapper->SetParameters(chain, parallel, parameter))
+    return NULL;   // Got parameter, but couldn't deal with it.
+  return mapper;
+}
+}  // namespace rgb_matrix
